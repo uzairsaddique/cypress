70d7100d8d8f379f81f72ede90eda5f9c2044dd9
uzairsaddique <uzairsaddique@gmail.com>
2021-12-30 01:38:26 +0500

    ousu

--- cypress/integration/testingincypress/conter.js
+++ cypress/integration/testingincypress/conter.js
@@ -14,7 +14,9 @@ describe("testing the main pat of the ", ()=>{
     });
     it("should not be  able to submit a successful via contact us from as from all fiedle are required" , ()=>{
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+
         cy.get('[name="first_name"]').type("uzair")
+        
         cy.get('[name="last_name"]').type("siddique")
         cy.get('[name="email"]').type("uzair@gmail.com")
         cy.get('textarea.feedback-input').type("something is not here")
